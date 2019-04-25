d28c320bc8dc8cceccdc676066d522cff175ac78
Gregory Setnik <gsetnik18@gmail.com>
2019-04-25 18:20:00 -0400

    test

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get(express.static(path.join(__dirname, './app/public')));
+app.use(express.static(path.join(__dirname, './app/data')));
+
 
 require('./app/routing/apiroutes')(app);
 require('./app/routing/htmlroutes')(app);
