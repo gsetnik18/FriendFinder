9592b99203f22a30d9b7ef4bdf0f76e4bca5f614
Gregory Setnik <gsetnik18@gmail.com>
2019-04-25 16:39:52 -0400

    test

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.get(express.static(path.join(__dirname, './app/public')));
 
-require('/app/routing/apiroutes')(app);
-require('/app/routing/htmlroutes')(app);
+require('./app/routing/apiroutes')(app);
+require('./app/routing/htmlroutes')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
