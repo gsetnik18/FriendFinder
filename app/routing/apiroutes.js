7ef22d0cef98b0daccd3f416da6bc28eb5037a54
Gregory Setnik <gsetnik18@gmail.com>
2019-04-27 01:00:52 -0400

    test to see if survey pushes info to friends list and matches friends

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -12,11 +12,28 @@ module.exports = function (app) {
     app.post('/api/friends', function (req, res) {
         var userIn = req.body;
         var userRes = userIn.scores;
-        //console.log(req.body.scores)
 
         // parseInt for scores
         for (var i = 0; i < userRes.scores.length; i++) {
             user.scores[i] = parseInt(userRes.scores[i]);
         };
+        var friendshipIndex = 0;
+        var minimumDifference = 400;
+
+        for(var i = 0; i < friends.length; i++) {
+            var difference = 0;
+            for(var j = 0; j < friends[i].scores.length; j++) {
+             difference += Math.abs(user.scores[j] - friends[i].scores[j]);
+
+              if(difference < minimumDifference) {
+                friendshipIndex = i;
+                minimumDifference = difference;
+              }
+            }
+
+            friends.push(userIn);
+
+            res.json(friends[friendshipIndex]);
+        }
     });
-};
+}
